8c741077f1726093275ade397cd8f1d059d6ee02
shibukihayato <shitayama14-20@au.com>
2019-09-09 14:40:46 +0900

    ９月９日 : End_canvace

--- New Unity Project/Assets/script/End_canvasController.cs
+++ New Unity Project/Assets/script/End_canvasController.cs
@@ -12,30 +12,42 @@ public class End_canvasController : MonoBehaviour
     public static float End_Life_Score = 0;
 
     public Text Text_Score = null;
+    public Text Text_All_Score = null;
     public Text Text_Time = null;
     public Text Text_Life = null;
 
     IEnumerator Stop_time()
     {
+        //Text_Score.text = "Score : " + End_Score.ToString();
         End_Time_Score = End_Score + End_Time;
-        Text_Score.text = "Score : " + End_Score.ToString();
-        Text_Time.text = "Time : " + End_Time.ToString("f0");
-        Text_Life.text = "Life : " + End_Life.ToString();
-        yield return new WaitForSeconds(2f);
-        Text_Score.text = "Score " + End_Score.ToString("f0") + "点 + Time " + End_Time.ToString("f0") + "秒 = " + End_Time_Score.ToString("f0");
-        End_Score = End_Time_Score;
         End_Life_Score = End_Score + (End_Life * 200);
-        yield return new WaitForSeconds(2f);
-        Text_Score.text = "Score " + End_Score.ToString("f0") + "点 + Life " + End_Life.ToString() + "×100 = " + End_Life_Score.ToString("f0");
+        Text_All_Score.text = " ";
+        Text_Score.text = "Score : " + End_Score.ToString() + " ポイント";
+        yield return new WaitForSeconds(1f);
+        Text_Life.text = "Life : " + End_Life.ToString() + " × 200 ";
+        yield return new WaitForSeconds(1f);
+        Text_Life.text = "Life : " + End_Life_Score.ToString() + " ポイント";
+        yield return new WaitForSeconds(1f);
+        Text_Time.text = "Time : " + End_Time.ToString("f0") + " × 1 ";
+        yield return new WaitForSeconds(1f);
+        Text_Time.text = "Time : " + End_Time.ToString("f0") + " ポイント";
+        //Text_Score.text = "Score " + End_Score.ToString("f0") + "点 + Time " + End_Time.ToString("f0") + "秒 = " + End_Time_Score.ToString("f0");
+        //End_Score = End_Time_Score;
+        yield return new WaitForSeconds(1f);
+        //Text_Score.text = "Score " + End_Score.ToString("f0") + "点 + Life " + End_Life.ToString() + "×100 = " + End_Life_Score.ToString("f0");
         End_Score = End_Life_Score;
-        yield return new WaitForSeconds(2f);
-        Text_Score.text = "Score : " + End_Score.ToString("f0") + " ポイント";
-        Text_Time.text = " ";
-        Text_Life.text = " ";
+        //yield return new WaitForSeconds(2f);
+        Text_All_Score.text = "Score : " + End_Score.ToString("f0") + " ポイント";
+        //Text_Time.text = " ";
+        //Text_Life.text = " ";
     }
 
     void Start()
     {
+        Text_Score.text = "　";
+        Text_Life.text = "　";
+        Text_Time.text = "　";
+        Text_All_Score.text = "　";
         StartCoroutine("Stop_time");
     }
 }
---
+++ New Unity Project/Assets/script/Transparent_parutena.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transparent_parutena : MonoBehaviour
+{
+    //public float alfa = 1f;
+    float Speed = 0.70f;
+    Color color;
+    bool Toumei = false;
+    bool Stop = false;
+
+    void Start()
+    {
+        Color color = gameObject.GetComponent<Renderer>().material.color;
+        color.r = 0.4f;
+        color.g = 1f;
+        color.b = 1f;
+        color.a = 0.5f;
+        gameObject.GetComponent<Renderer>().material.color = color;
+        Toumei = false;
+        Stop = false;
+       // GetComponent<Image>().color = new Color(0f, 0f, 0f, alfa);
+    }
+    IEnumerator Parutena_Fade()
+    {
+        Stop = true;
+        Toumei = true;
+        Debug.Log("true");
+        yield return new WaitForSeconds(1.5f);
+        Toumei = false;
+        Debug.Log("1,5秒");
+        yield return new WaitForSeconds(1.5f);
+        Stop = false;
+        Toumei = true;
+        Debug.Log("3秒");
+    }
+
+    void Update()
+    {
+        color.r = 0.4f;
+        color.g = 1f;
+        color.b = 1f;
+        Debug.Log(color.a);
+        if (Toumei == true)
+        {
+            if (Stop == false)
+            {
+                StartCoroutine("Parutena_Fade");
+            }
+            color.a += Speed * Time.deltaTime;
+            gameObject.GetComponent<Renderer>().material.color = color;
+            //GetComponent<Image>().color = new Color(0f, 0f, 0f, alfa);
+           // alfa -= Speed * Time.deltaTime;
+        }
+        else
+        {
+            if (Stop == false)
+            {
+                StartCoroutine("Parutena_Fade");
+            }
+            color.a -= Speed * Time.deltaTime;
+            gameObject.GetComponent<Renderer>().material.color = color;
+            //GetComponent<Image>().color = new Color(0f, 0f, 0f, alfa);
+            //alfa += Speed * Time.deltaTime;
+        }
+
+    }
+
+}
